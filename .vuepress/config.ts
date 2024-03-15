b1f960e14b067f21e00135808e5603e73185eed1
zengjielin <248717607@qq.com>
2024-03-15 17:29:52 +0800

    feat: add article content

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -25,96 +25,15 @@ export default defineUserConfig({
     docsBranch: "main",
     docsDir: "example",
     lastUpdatedText: "",
-    // series 为原 sidebar
-    // series: {
-    //   "/docs/theme-reco/": [
-    //     {
-    //       text: "module one",
-    //       children: ["home", "theme"],
-    //     },
-    //     {
-    //       text: "module two",
-    //       children: ["api", "plugin"],
-    //     },
-    //   ],
-    // },
-    navbar: [
-      // { text: "Home", link: "/" },
-      // { text: "Categories", link: "/categories/reco/1/" },
-      // { text: "Tags", link: "/tags/tag1/1/" },
-      // {
-      //   text: "Docs",
-      //   children: [
-      //     { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
-      //     { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
-      //   ],
-      // },
-    ],
-    autoAddCategoryToNavbar: true, // 自动将分类和标签添加至头部导航条
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: "text",
-    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "QQ 群",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1：1037296104</li>
-    //         <li>QQ群2：1061561395</li>
-    //         <li>QQ群3：962687802</li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "GitHub",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "buttongroup",
-    //       children: [
-    //         {
-    //           text: "打赏",
-    //           link: "/docs/others/donate.html",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-    // 设置单个密码
-    // password: "14e1b600b1fd579f47433b88e8d85291",
-    // 设置多个密码
-    // password: [
-    //   "14e1b600b1fd579f47433b88e8d85291",
-    //   "f8de1968939dd4ac5992ce962993ac2b",
-    // ],
+    autoSetBlogCategories: true,
+    // 自动将分类和标签添加至头部导航条
+    autoAddCategoryToNavbar: {
+      location: 1, // 默认 0
+      categoryText: '分类', // 默认 categories
+      tagText: '标签' // 默认 tags
+    },
     commentConfig: {
       type: "valine",
-      // options 与 1.x 的 valineConfig 配置一致
       options: {
         appId: "7Rd5srHP9piH7CCJ62sKuAW2-gzGzoHsz",
         appKey: "9CZeENYCYA02EouZB0WWy3D1",
